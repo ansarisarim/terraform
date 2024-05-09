06504028729dd791312a824ccb52bcd8883107d7
sarim <sarim2@gmail.com>
2024-05-09 09:35:24 +0000

    ec2

---
+++ matchin-conf.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region     = "ap-south-1"
+  profile    = "default" 
+
+}
+
+resource "aws_instance" "terra-EC2" {
+  ami             = "ami-0f58b397bc5c1f2e8"
+  instance_type   = "t2.micro"
+  security_groups = ["default"]
+  key_name        = "abc.pem"
+}
+
